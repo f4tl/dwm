d4ed4018820e8cf22c3eb97085cf9eb1c3d8bc84
f4tl <murilo0x01@gmail.com>
2025-11-04 23:34:32 -0300

    Adding the right path for the wallpaper!

--- startdwm.sh
+++ startdwm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 exec slstatus &
-exec feh --bg-scale /home/fatal/Downloads/wallpaper/atacadao.png  &
+exec feh --bg-scale ~/images/girl.jpeg  &
 exec /usr/local/bin/clipboard_fix.sh &
 exec oneko &
 exec alacritty --daemon &
