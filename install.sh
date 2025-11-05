0d790ab0c8b89380f8ac3ec3dcf5f65a2abde13a
f4tl <murilo0x01@gmail.com>
2025-11-04 23:31:23 -0300

    wow! install scripts!!

---
+++ install.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+sudo apt install build-essential libx11-dev libxft-dev libxinerama-dev libfreetype6-dev libfontconfig1-dev feh alacritty fish xfonts-terminus
+mv dwm/ ~
+mv slstatus/ ~
+
+mv clipboard_fix.sh /usr/local/bin
+mv startdwm.sh /usr/local/bin
+
+chmod +x /usr/local/bin/startdwm.sh
+chmod +x /usr/local/bin/clipboard.sh
+
+mv dwm.desktop /usr/share/xsessions/
+
+# Setuping DWM
+
+cd ~/dwm 
+sudo make clean install
+cd ~/slstatus
+sudo make clean install
