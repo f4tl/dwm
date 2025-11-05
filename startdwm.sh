2b1f03e9a8d403563ba61bbaf21d9caf760eabf4
f4tl <murilo0x01@gmail.com>
2025-11-04 23:25:20 -0300

    Three important scripts that i use.

---
+++ startdwm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+exec slstatus &
+exec feh --bg-scale /home/fatal/Downloads/wallpaper/atacadao.png  &
+exec /usr/local/bin/clipboard_fix.sh &
+exec oneko &
+exec alacritty --daemon &
+exec dwm
